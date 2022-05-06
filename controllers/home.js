7c60c64acf8415eab81e5998a773c330dec41a08
Namees Albayati <nameesalbayati@Nameess-MacBook-Pro.local>
2022-05-06 13:12:25 -0500

    adding contact info

--- controllers/home.js
+++ controllers/home.js
@@ -1,17 +1,28 @@
 const path = require('path');
-const router=require('express').Router();
-router.get('/',async(req,res)=>{
-    res.sendFile(path.join(__dirname,"../views/index.html"))
+const router = require('express').Router();
+const fs = require("fs");
+const PdfReader = require('pdfreader').PdfReader;
+router.get('/', async (req, res) => {
+    res.sendFile(path.join(__dirname, "../views/index.html"))
 })
-router.get('/about',async(req,res)=>{
-    res.sendFile(path.join(__dirname,"../views/about.html"))
-})
-
-router.get('/projects',async(req,res)=>{
-    res.sendFile(path.join(__dirname,"../views/projects.html"))
+router.get('/about', async (req, res) => {
+    res.sendFile(path.join(__dirname, "../views/about.html"))
 })
 
-router.get('/contact',async(req,res)=>{
-    res.sendFile(path.join(__dirname,"../views/contactme.html"))
+router.get('/projects', async (req, res) => {
+    res.sendFile(path.join(__dirname, "../views/projects.html"))
 })
-module.exports=router
+
+router.get('/contact', async (req, res) => {
+    res.sendFile(path.join(__dirname, "../views/contactme.html"))
+})
+
+router.get('/pdf', async (req, res) => {
+        res.sendFile(path.join(__dirname,'../public/pdf/namees.pdf'))
+
+ 
+
+
+})
+
+module.exports = router
